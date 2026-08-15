0780df82167ec77ef50c0aa883e48be03ac8f77f
sunnyD1000 <118693960+sunnyD1000@users.noreply.github.com>
2026-08-14 20:01:40 -0500

    Hysteresis Threshold 40x120

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ static void detectPaperEdges(cv::Mat &bgr)
 	cv::Mat gray, edges;
 	cv::cvtColor(bgr, gray, cv::COLOR_BGR2GRAY);
 	cv::GaussianBlur(gray, gray, cv::Size(5, 5), 0);
-	cv::Canny(gray, edges, 50, 150);
+	cv::Canny(gray, edges, 40, 120);
 
 	std::vector<std::vector<cv::Point>> contours;
 	cv::findContours(edges, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
